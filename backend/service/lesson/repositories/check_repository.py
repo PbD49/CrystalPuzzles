06e4a979e5c5d79d01d62437b2dd8acca68946f9
srs <rs.sku@yandex.ru>
2024-08-09 01:01:20 +0300

    Update check routers, schemas, repo

--- backend/service/lesson/repositories/check_repository.py
+++ backend/service/lesson/repositories/check_repository.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 from sqlalchemy import insert
 
-from common.repository.base_repository import BaseRepository
-from service.lesson.models import Check
+from common.repository.base_repository import BaseRepository, TModel
+from service.lesson.models import Check, Lesson
 from service.lesson.schemas.check_schema import CheckFilterSchema
-from sqlalchemy import select, or_
+from sqlalchemy import select
 from sqlalchemy.orm import joinedload
 
 
@@ -15,7 +17,7 @@ class CheckRepository(BaseRepository):
         result = (await self.session.execute(stmt)).scalar_one_or_none()
         return result
 
-    async def get_all_check_by_filter(self, filters: CheckFilterSchema):
+    async def get_all_check_by_filter(self, filters: CheckFilterSchema) -> dict:
         stmt = (
             select(self.model)
             .options(joinedload(self.model.training_data))
@@ -25,7 +27,7 @@ class CheckRepository(BaseRepository):
         if filters.date_add:
             stmt = stmt.filter(self.model.date_add == filters.date_add.date())
         if filters.trainer:
-            stmt = await self._add_filters(stmt, trainer_id=filters.trainer)
+            stmt = stmt.filter(self.model.lesson.has(Lesson.trainer_id == filters.trainer))
         if filters.student:
             stmt = await self._add_filters(stmt, student_id=filters.student)
 
@@ -33,3 +35,16 @@ class CheckRepository(BaseRepository):
         records = await self._get_records(count_records, stmt, filters)
         response = await self._convert_response(count_records, records, filters)
         return response
+
+    async def get(self, check_id: int) -> Optional[TModel]:
+        stmt = (
+            select(self.model)
+            .filter(
+                self.model.id == check_id,
+                self.model.deleted.__eq__(False))
+            .options(
+                joinedload(self.model.lesson)
+            )
+        )
+        result = (await self.session.execute(stmt)).scalar_one_or_none()
+        return result
